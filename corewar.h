a7dd961f479964d4155fc1eca06ef94f0fb083e0
Nick <nekit_p@ukr.net>
2017-10-09 19:57:43 +0300

    structs moved temporary to nekit_main.c

--- corewar.h
+++ corewar.h
@@ -17,36 +17,36 @@ typedef struct		s_op
 	int				label_size;
 }					t_op;
 
-typedef struct		s_proc
-{
-	size_t			pc;
-	int				reg[REG_NUMBER + 1];
-	char			carry;
-	struct s_proc	*next;
-}					t_proc;
+// typedef struct		s_proc
+// {
+// 	size_t			pc;
+// 	int				reg[REG_NUMBER + 1];
+// 	char			carry;
+// 	struct s_proc	*next;
+// }					t_proc;
 
-typedef struct		s_player
-{
-	char			*name;
-	char			*comment;
-	unsigned char	*program_code;
-	size_t			last_live;
-	unsigned int	sum_lives;
-	int				num;
-	int				code_size;
-}					t_player;
+// typedef struct		s_player
+// {
+// 	char			*name;
+// 	char			*comment;
+// 	unsigned char	*program_code;
+// 	size_t			last_live;
+// 	unsigned int	sum_lives;
+// 	int				num;
+// 	int				code_size;
+// }					t_player;
 
-typedef struct		s_core
-{
-	size_t			cycle;
-	size_t			gen_lives_in_current_period;
-	size_t			cycle_to_die;
-	t_player		**players;
-	int				num_of_players;
-	int				processes;
-	int				cycles_per_second;
-	char			paused;
-	unsigned char	field[MEM_SIZE];
-}					t_core;
+// typedef struct		s_core
+// {
+// 	size_t			cycle;
+// 	size_t			gen_lives_in_current_period;
+// 	size_t			cycle_to_die;
+// 	t_player		**players;
+// 	int				num_of_players;
+// 	int				processes;
+// 	int				cycles_per_second;
+// 	char			paused;
+// 	unsigned char	field[MEM_SIZE];
+// }					t_core;
 
 #endif
