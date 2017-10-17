425ce3e9296d66f0c85e70ed6a1765d9dc798ace
Yaroslav Robotko <yrobotko@e3r2p4.unit.ua>
2017-10-17 19:23:26 +0300

    fix bugs. beta v.0.5

--- assembler/assembler/asm.c
+++ assembler/assembler/asm.c
@@ -6,7 +6,7 @@
 /*   By: yrobotko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/16 14:17:46 by yrobotko          #+#    #+#             */
-/*   Updated: 2017/10/16 18:24:59 by yrobotko         ###   ########.fr       */
+/*   Updated: 2017/10/17 19:15:19 by yrobotko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,32 @@ int				main(int argc, char **argv)
 	close(fd);
 	free(start);
 	return (0);
+}
+
+long long		fft_atoi(const char *str)
+{
+	long long	result;
+	int			is_neg;
+	int			i;
+
+	result = 0;
+	is_neg = 0;
+	i = 0;
+	while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
+			|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
+		i++;
+	if (str[i] == 45)
+		is_neg = 1;
+	if ((str[i] == 45) || (str[i] == 43))
+		i++;
+	while ((str[i] >= 48) && (str[i] <= 57))
+	{
+		result *= 10;
+		result += ((int)str[i] - 48);
+		i++;
+	}
+	if (is_neg)
+		return (-result);
+	else
+		return (result);
 }
