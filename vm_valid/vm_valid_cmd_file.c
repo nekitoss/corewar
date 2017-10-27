612ca272c55e51469a5ada2b67ff8738c9d1ea1f
Maxim Kurchinskyj <mkurchin@e2r11p14.unit.ua>
2017-10-27 20:30:33 +0300

    v

--- vm_valid/vm_valid_cmd_file.c
+++ vm_valid/vm_valid_cmd_file.c
@@ -43,13 +43,19 @@ int		ft_isdigit_str(char *argv)
 void sv_number_player(char *argv, t_arg *ptr, int *fl)
 {
 	int tmp;
+	size_t num;
 
 	if (ft_isdigit_str(argv))
 		ft_exit("Not correct value of number player, my friend\n");
 	if (ft_strlen(argv) > 10)
 		ft_exit("Error: too big number, my friend, are you joke?)\n");
+	num = (size_t)ft_atoi(argv);
+	if (ft_strcmp(argv, ft_itoa(num)))
+		ft_exit("Error: to big number, my friend\n");
+	if (num > 2147483647)
+		ft_exit("Error: to big number, my friend\n");
 	tmp = ft_atoi(argv);
-	if (tmp == 0)
+	if (tmp == 0 || tmp > MAX_PLAYERS)
 		ft_exit("Error: number must be bigger than zero, my friend");
 	ptr->num[ptr->cnt_player] = tmp;
 	(*fl)++;
